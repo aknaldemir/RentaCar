ef35c4a822dce7bc194e8c4b772f446458c54117
fgulpolat <49431870+fgulpolat@users.noreply.github.com>
2019-10-26 13:59:09 +0300

    Update Arac.cs

--- RentACar.Entity/Arac.cs
+++ RentACar.Entity/Arac.cs
@@ -16,16 +16,6 @@ namespace RentACar.Entity
         public int ModelId { get; set; }
         public Model Model{ get; set; }
         public List<Resim> Resimler { get; set; }
-        ///dfdfdfdfdfdf
-        public int MyProperty { get; set; }
-
-<<<<<<< HEAD
-        public string Degisiklik { get; set; }
-
-        public int deneeeeeeee { get; set; }
-
-=======
-        public string deneme { get; set; }
->>>>>>> 3fb229801f28f9adb4c51c25ae67c845f9700c0e
+        
     }
 }
